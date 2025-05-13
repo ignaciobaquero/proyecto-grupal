c754a5d04cb4f0e6ef3f3e23c890118f2c96dbb7
Camilo <Amadogomez2006@gmail.com>
2025-05-12 21:38:10 -0300

    Login falso v0.0

---
+++ js/login.js
@@ -0,0 +1,10 @@
+//alerta de iniciar sesion
+
+
+window.addEventListener("load",function(){
+    document.getElementById("viewAlert2").addEventListener("click", function(){
+        alert("No se ha podido encontrar una cuenta vinculada con ese nombre de usuario.")
+    })
+
+
+})
